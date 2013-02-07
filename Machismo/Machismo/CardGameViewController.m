82ad8116ce6090c3032cc1f094adec9fc48c9d14
Ken Ramsey <ken_ramsey@icloud.com>
2013-02-07 00:13:57 -0500

    hw2 (Set) - Started on model. Matching alorithm done.

--- Machismo/Machismo/CardGameViewController.m
+++ Machismo/Machismo/CardGameViewController.m
@@ -122,7 +122,7 @@
 - (IBAction)flipCard:(UIButton *)sender
 {
     [self.game flipCardAtIndex:[self.cardButtons indexOfObject: sender]];
-    self.flipCount++;
+    if (!sender.isSelected && sender.isEnabled) self.flipCount++;
     [self.matchNumSwitch setEnabled:NO];
 
     [self updateUI];
